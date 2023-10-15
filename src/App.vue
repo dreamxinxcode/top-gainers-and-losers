c5d116e5d45715ff9dc22c1a2ebbd0990c245cf5
dreamxinxcode <dreamxinxcode@gmail.com>
2023-10-15 16:52:45 +0000

    Make API call, handle for errors

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,34 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted } from 'vue';
+import { Ticker } from './types/Ticker'
+
+const topGainers = ref<Ticker[]>([]);
+const topLosers = ref<Ticker[]>([]);
+const mostActivelyTraded = ref<Ticker[]>([]);
+const lastUpdated = ref('');
+
+const fetchMarketData = async () => {
+  try {
+    const response = await fetch('https://www.alphavantage.co/query?function=TOP_GAINERS_LOSERS&apikey=demo');
+    const data = await response.json();
+    topGainers.value = data.top_gainers;
+    topLosers.value = data.top_losers;
+    mostActivelyTraded.value = data.most_actively_traded;
+    lastUpdated.value = data.last_updated;
+  } catch(error) {
+    console.error('Error:', error);
+  }
+};
+
+onMounted(() => {
+  fetchMarketData();
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
+
 </template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+<style scoped lang="scss">
+
 </style>
