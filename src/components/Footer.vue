16f99543a00cb5ad2f9d663518b9ddbaee35c33d
dreamxinxcode <dreamxinxcode@gmail.com>
2023-10-15 20:57:51 +0000

    Add Footer component with refresh, last updated and link to repo + cleaner props syntax

---
+++ src/components/Footer.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+defineProps<{ lastUpdated: string }>();
+</script>
+
+<template>
+  <footer>
+    <span>
+      <font-awesome-icon @click="$emit('refreshData')" :icon="['fa', 'arrows-rotate']" />
+    </span>
+    <span>
+      Last updated: {{ lastUpdated }}
+    </span>
+    <a href="https://github.com/dreamxinxcode/top-gainers-and-losers/" target="_blank">
+      <span>
+        <font-awesome-icon :icon="['fa-brands', 'fa-github-alt']" />
+      </span>
+    </a>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+footer {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  height: 5rem;
+}
+</style>
